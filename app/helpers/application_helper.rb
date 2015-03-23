ba357d736b14d7d9f1c3a64984cf9cedfc170806
Gianna Fusaro <giannafusaro389@gmail.com>
2015-03-22 22:35:47 -0700

    Create icon_link for anchors with just an icon (with optional text)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def icon_link(icon = nil, html_options = {}, &block)
+    icon_text = html_options.delete(:icon_text)
+    html_options.transform_keys! { |key| /data_(\w+)/ === key ? key.to_s.dasherize : key }
+    content_tag(:a, nil, html_options) { content_tag(:i, icon_text, class: icon) }
+  end
+
 end
