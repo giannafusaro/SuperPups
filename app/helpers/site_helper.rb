5860f7246f7b25d4e92feb01916bb09089d14e0d
Gianna Fusaro <giannafusaro389@gmail.com>
2015-04-08 02:15:21 -0700

    Create review_details and bg (inline CSS) helpers

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,11 @@
 module SiteHelper
+
+  def reviewer_details(reviewer)
+    content_tag(:figure, nil, style: "background-image:url(#{reviewer[:photo_url]})") +
+    content_tag(:figcaption, reviewer[:name])
+  end
+
+  def bg(photo_url)
+    "background-image: url(#{photo_url});"
+  end
 end
