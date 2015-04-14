ed79ded0e501768b4c32cd68a487c098a4bdf389
Jeff Miller <jeff@humani.se>
2015-04-13 19:03:11 -0700

    Add icon and background helpers to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
 
+  def bg(photo_url)
+    "background-image: url(#{photo_url});"
+  end
+
+  def icon(icon, icon_text=nil)
+    content_tag(:i, icon_text, class: "superpups-#{icon}")
+  end
+
   def icon_link(icon = nil, html_options = {}, &block)
     icon_text = html_options.delete(:icon_text)
     html_options.transform_keys! { |key| /data_(\w+)/ === key ? key.to_s.dasherize : key }
-    content_tag(:a, nil, html_options) { content_tag(:i, icon_text, class: icon) }
+    content_tag(:a, nil, html_options) { icon(icon, icon_text) }
   end
 
 end
